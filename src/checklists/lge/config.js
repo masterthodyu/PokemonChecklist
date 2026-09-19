9f65a513d47d019a677fa7a0fdbc5815e8666b37
Anthony Hoang <58443879+masterthodyu@users.noreply.github.com>
2026-09-19 05:59:40 -0400

    Quick typo in config (#28)

--- src/checklists/lge/config.js
+++ src/checklists/lge/config.js
@@ -2,7 +2,7 @@ import data from './data.json'
 export default {
   id: 'lge',
   title: "Pokémon Let's Go Eevee ",
-  path: '/lgp',
+  path: '/lge',
   icon: 'https://assets.nintendo.eu/image/upload/f_auto,c_limit,w_992,q_auto:low/MNS/NOE/70010000000449/SQ_NSwitch_PokemonLetsGoEevee_enGB',
   accentFrom: '#c7ccd1',
   accentTo: '#f4c430',
